8126557cce6da816a3c4c83db3bee6f564267ab2
Alexandre Louf <contact@alexandrelouf.fr>
2019-04-12 00:35:10 +0200

    étape 4 et 5

--- code/index.php
+++ code/index.php
@@ -1,2 +1,29 @@
 <?php
-new PDO('mysql:dbname=test;host=docker_mariadb_1;port=3307;charset=utf8', 'myuser', 'mypswd');
+$conn = new PDO('mysql:dbname=counter;host=dockerbasic_mariadb_1;port=3306', 'root', 'root');
+
+$stmt = $conn->prepare("SELECT * FROM counter WHERE id = 1");
+$stmt->execute();
+$row = $stmt->fetch();
+
+echo $row["counter"];
+
+$counter = $row["counter"] + 1;
+
+$sql = "UPDATE counter SET counter=? WHERE id=1";
+$stmt = $conn->prepare($sql);
+$stmt->execute([$counter]);
+
+$stmt = $conn->prepare("SELECT * FROM counter WHERE id = 1");
+$stmt->execute();
+$row = $stmt->fetch();
+
+echo $row["counter"];
+
+?>
+
+Vous avez rafraichi la page <?= $counter ?> fois
+
+<br>
+<br>
+
+Voir le PHP INFO (exercice 4) : <a href="phpinfo.php">Lien</a>
